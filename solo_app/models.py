fe7897f258ba52a0871ecf23dcc343aa3d5691a7
Computerblue <ohsheet2373@gmail.com>
2021-05-27 22:23:08 -0700

    solo proj new

--- solo_app/models.py
+++ solo_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User (models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    email_address = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
